e69ec3e3bc18564836bcd03fb2d7af182de13895
Brian Espinosa <b@bje.co>
2025-08-06 17:23:39 -0700

    chore: update lint tools

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
-import unicorn from 'eslint-plugin-unicorn';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import jsonFormat from 'eslint-plugin-json-format';
+import pluginJest from 'eslint-plugin-jest';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
@@ -16,27 +15,35 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ['*', '!src', '!**/package.json', '**/.pnp.*', '**/node_modules'],
+    ignores: [
+      '*',
+      '!src',
+      '!**/package.json',
+      '**/.pnp.*',
+      '**/node_modules',
+      '**/coverage',
+    ],
   },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:unicorn/recommended',
-    'next',
-    'prettier',
-  ),
+  ...compat.extends('eslint:recommended', 'next', 'prettier'),
   {
     plugins: {
-      unicorn,
       'simple-import-sort': simpleImportSort,
-      'json-format': jsonFormat,
+      jest: pluginJest,
     },
 
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
+      globals: pluginJest.environments.globals.globals,
     },
 
     rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+
       'react/self-closing-comp': [
         'error',
         {
@@ -57,27 +64,6 @@ export default [
             ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
             ['^\\u0000'],
           ],
-        },
-      ],
-
-      'unicorn/filename-case': [
-        'error',
-        {
-          cases: {
-            kebabCase: true,
-            pascalCase: true,
-          },
-        },
-      ],
-
-      'unicorn/prevent-abbreviations': [
-        'error',
-        {
-          replacements: {
-            props: {
-              properties: false,
-            },
-          },
         },
       ],
     },
